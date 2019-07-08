0ca77033281a3fa64808d72e94736ee70b0fba44
Michal Kukielka <michal.kukielka@creadis.com>
2019-07-08 13:47:10 +0200

    added exercise 3

--- Exercises_2.py
+++ Exercises_2.py
@@ -2,5 +2,12 @@
  # https://www.py4e.com/html3/02-variables
  # Exercis 2:
 
-name = input("What is your name:")
+name = input("What is your name:\n")
 print ("Hello "+name)
+
+hours = input("Enter Hours:\n")
+rate = input("Enter Rate: \n")
+pay = float(hours) * float(rate)
+
+print(pay)
+input("Press Enter kay to end")
