d59a30793ea781dfa19de144cf3f7e9ec3d34cff
Michal Kukielka <michal.kukielka@creadis.com>
2019-07-11 09:12:28 +0200

    added function

--- Exercises_2.py
+++ Exercises_2.py
@@ -1,20 +1,23 @@
  # Exercises, part 2 Variables: from 2- 5
  # https://www.py4e.com/html3/02-variables
- # Exercis 2:
+ # Exercis 2->Exrcies 6: from part 4.
 
+
+def pay_rate(hours,rate):
+    pay = hours * rate
+    return pay
+    
 name = input("What is your name:\n")
 print ("Hello "+name)
 
-
-hours = input("Enter Hours:\n")
-rate = input("Enter Rate: \n")
-
 try:
-    pay = float(hours) * float(rate)
+    priv_hours = float(input("Enter Hours:\n"))
+    priv_rate = float(input("Enter Rate: \n"))  
+    priv_pay = pay_rate(priv_hours,priv_rate)
+    print(priv_pay)
 except:
     print ("Please enter a number")
 # add reconies if the user niput was word or coma    
 
-print(pay)
 input("Press Enter to terminate")
  
