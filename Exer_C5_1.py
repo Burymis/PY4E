0a7b520a43227f0c01c2c1e9928ba78e33fc03b5
Michal Kukielka <michal.kukielka@creadis.com>
2019-07-30 10:27:26 +0200

    Add exercise 5

--- Exer_C5_1.py
+++ Exer_C5_1.py
@@ -1,35 +1,11 @@
- # Exercises, part 5 Iteration
+ # Exercises, part 6 strings
  # https://www.py4e.com/html3/05-iterations
- # Exercis 1->2:
+ # Exercis 5:
 
+str1 = 'X-DSPAM-Confidence:0.8475'
 
-count = 0
-total = 0
-min_numb = None
-max_numb = None
+data=float(str1[str1.find('0'):])
+print(data)
 
-while True:
-    imp=input("Enter a number: ")
-    if imp == "done":
-        break
-    try:
-        imp_numb=float(imp)
-    except:
-        print("Invalid input")
-        continue
-    if min_numb == None or imp_numb < min_numb: #store the min value
-        min_numb = imp_numb
-        
-    if max_numb == None or imp_numb > max_numb: #store the max value
-        max_numb = imp_numb
-    
-    count = count +1
-    total = total + imp_numb
-    average  = total/count
-    print("trial: ", count," Total: ", total, " average: ", average)
-    
-print("Final score:")
-print("Trials: ", count," Total: ", total, " Average: ", average)
-print("Min value: ", min_numb," Max value: ", max_numb)
-
+input("Press enter to end ")
 
